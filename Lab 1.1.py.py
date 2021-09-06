859d5b9787215f29c4be7a6a84b383f9fe47929a
1201200309-AlvinChen <90165856+1201200309-AlvinChen@users.noreply.github.com>
2021-09-06 10:55:23 +0800

    Add files via upload

---
+++ Lab 1.1.py.py
@@ -0,0 +1,9 @@
+#StudentID: 1201200309#
+#Student Name: Alvin #
+
+print("I like Phyton")
+
+person = input("Please enter your name: ")
+print("Hello", person)
+
+
---
+++ Lab 1.2.py.py
@@ -0,0 +1,16 @@
+# Lab 1.2 get the two numbers and display the sum of the two numbers
+
+number_1 = int(input("Please enter number 1:"))
+number_2 = int(input("Please enter number 2:"))
+
+total = number_1 + number_2
+
+print("The total number is :", total)
+
+# print("The total number is ", total)
+# Display --> The sum of 5 and 6 is 11
+
+print("The sum is ",total," ",number_1,"and",number_2)
+print("number_1 is :",number_1)
+print("number_2 is :",number_2)
+print("total :",total)
